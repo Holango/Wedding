b0e8cb9797fbfe731d8ccc8af465196f10d6d4a6
Holango <holango_hi@naver.com>
2025-04-20 16:25:35 +0900

    config_fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 여기를 정확히 확인합니다.
+  base: '/Wedding/', // 리포지토리 이름 앞뒤에 슬래시(/)가 있어야 합니다.
 })
