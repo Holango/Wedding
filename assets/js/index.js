e1089a11c08879d2e6cee6bb29db1a7c5c1a60e2
thumb-yeongo <140412872+thumb-yeongo@users.noreply.github.com>
2025-07-31 21:09:20 +0900

    js 분리 및 카카오 공유하기 테스트

---
+++ assets/js/index.js
@@ -0,0 +1,57 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
+function copyCont() {
+    const copyBtns = vdocument.querySelectorAll('.btn-copy')
+    copyBtns.forEach(function (btn) {
+        btn.addEventListener('click', function () {
+            const copyCont = this.closest('.a-item').querySelector('.copy-cont');
+            const copyText = copyCont?.textContent.trim();
+
+            if (!copyText) {
+                alert('복사할 내용이 없습니다.');
+                return;
+            }
+
+            navigator.clipboard.writeText(copyText)
+                .then(() => {
+                    alert('복사되었습니다: ' + copyText);
+                })
+                .catch(err => {
+                    alert('복사 실패: ' + err);
+                });
+        });
+    });
+}
+function slideToggle() {
+    const triggers = document.querySelectorAll('.t-trigger')
+    triggers.forEach(function (trigger) {
+        trigger.addEventListener('click', function () {
+            const item = this.closest('.t-item');
+            const cont = item.querySelector('.t-cont');
+
+            // slideToggle 구현
+            if (cont.style.maxHeight) {
+                cont.style.maxHeight = null;
+            } else {
+                cont.style.maxHeight = cont.scrollHeight + 'px';
+            }
+
+            this.classList.toggle('active');
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    slideToggle();
+    copyCont();
+});
